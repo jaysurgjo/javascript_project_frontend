b3cc417311fd976cfdf1f60fba1d18fe214633d7
jordan surgick <jordansurgick27@gmail.com>
2022-06-13 17:54:38 -0400

    added create card method

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 createHeader();
 createFooter();
+createCard();
 
 function createHeader() {
   const createHead = document.createElement("HEADER");
@@ -30,4 +31,36 @@ otherUserButton.innerHTML = `Other User Entries`;
 document.getElementById("button_container").appendChild(homeButton);
 document.getElementById("button_container").appendChild(otherUserButton);
 
-function createCard() {}
+function getData() {
+  const first_name_element = document.getElementById("first_name");
+  const last_name_element = document.getElementById("last_name");
+  const location_element = document.getElementById("location");
+
+  return {
+    first_name: first_name_element.value,
+    last_name: last_name_element.value,
+    locatation: location_element.value,
+  };
+}
+
+function createCard(obj) {
+  //const { first_name, last_name, locatation } = getData();
+
+  const cardContent = document.createElement("tr");
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+
+  editButton.innerHTML = `Edit`;
+  deleteButton.innerHTML = `Delete`;
+
+  cardContent.innerHTML = `<td>Title of Drink PlaceHolder</td>
+                           <td>Recipe Example</td>`;
+
+  //cardContent.innerHTML[0] = `Title of Drink PlaceHolder`;
+  //cardContent.innerHTML[1] = `Recipe Example`;
+
+  cardContent.appendChild(editButton);
+  cardContent.appendChild(deleteButton);
+
+  document.getElementById("card_container").appendChild(cardContent);
+}
