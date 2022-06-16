6bd1d533968fd1bddd8923ef92802f2f431e8f8c
Meebly <hagpoh@gmail.com>
2022-06-16 10:29:55 -0700

    added back in database funtionality

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-document.getElementById("submit-form").addEventListener("click", function (e) {
-  e.preventDefault();
-  // Getting form and adding eventlistener to it for onclick with an async function, also preventing default so it doesnt refresh.
-
-  getFetchData();
-  document.querySelector(".content-wrapper").style.justifyContent =
-    "space-evenly";
-  // calling getFetchData and changing the styling for that query selector.
-});
-
-function getData() {
-  const first_name_element = document.getElementById("first_name");
-  const last_name_element = document.getElementById("last_name");
-  const location_element = document.getElementById("location");
-
 document.getElementById('submit-form').addEventListener('click', postData)
+
+function getData () {
+  const first_name_element = document.getElementById('first_name')
+  const last_name_element = document.getElementById('last_name')
+  const location_element = document.getElementById('location')
+
+  document.getElementById('submit-form').addEventListener('click', postData)
 
   return {
     first_name: first_name_element.value,
     last_name: last_name_element.value,
-    location: location_element.value,
+    location: location_element.value
     // Getting the data from the input form and returning the values.
-  };
+  }
 }
 
 function createCard (firstName, lastName, data) {
@@ -43,20 +35,9 @@ function createCard (firstName, lastName, data) {
           <p>${location} </p>
         </div>
 
-    </div>`;
-  document.querySelector(".content-right").appendChild(cardContent);
-  // Getting the data from the inputs, creating an img varible, creating div element, setting an attribute to that div. Creating innerhtml and using string interpolation to pass in the data to show on our card. Appedning card_content to the div element.
-}
-
-function createTable(obj) {
-  const { id, name, email } = obj;
-  const tableContent = document.createElement("table");
-  tableContent.className = "user_table";
-  // Creating obj from data, creating table, setting attribute to table.
-=======
     </div>`
-
   document.querySelector('.content-right').appendChild(cardContent)
+  // Getting the data from the inputs, creating an img varible, creating div element, setting an attribute to that div. Creating innerhtml and using string interpolation to pass in the data to show on our card. Appedning card_content to the div element.
 }
 
 function createTable (obj) {
@@ -84,9 +65,8 @@ function createTable (obj) {
       <td>Recipe item</td>
       <td class="delete_table">Delete</td>
     </tr>
-  </tbody>`;
-  // Creating innerhtml for the table and adding the delete button.
   </tbody>`
+  // Creating innerhtml for the table and adding the delete button.
 
   document.querySelector('.content-right').appendChild(tableContent)
   document
@@ -131,5 +111,7 @@ function postData (event) {
     .then(data => {
       createCard(first_name, last_name, data)
       document.getElementById('form_ele').reset()
+      document.querySelector('.content-wrapper').style.justifyContent =
+        'space-evenly'
     })
 }
